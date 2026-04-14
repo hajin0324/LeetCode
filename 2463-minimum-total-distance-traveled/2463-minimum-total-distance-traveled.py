8f9109a6c6a989db6093db78bf374dfeecc39931
hajin <74534952+hajin0324@users.noreply.github.com>
2026-04-14 22:18:02 +0900

    Time: 553 ms (69.14%), Space: 19.5 MB (86.42%) - LeetHub

--- 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.py
+++ 2463-minimum-total-distance-traveled/2463-minimum-total-distance-traveled.py
@@ -3,24 +3,24 @@ class Solution:
         robot.sort()
         factory.sort()
 
-        slots = []
-        for pos, limit in factory:
-            slots.extend([pos] * limit)
+        n = len(robot)
+        m = len(factory)
+        INF = 10**18
 
-        R, F = len(robot), len(slots)
-        INF = 10**20
+        dp = [[INF] * (m + 1) for _ in range(n + 1)]
 
-        dp = [[INF] * (F + 1) for _ in range(R + 1)]
-        
-        for j in range(F + 1):
-            dp[0][j] = 0  
+        for j in range(m + 1):
+            dp[0][j] = 0
 
-        for i in range(1, R + 1):
-            for j in range(1, F + 1):
-                dp[i][j] = dp[i][j-1]
+        for j in range(1, m + 1):
+            pos, limit = factory[j - 1]
 
-                if i <= j:
-                    cost = abs(robot[i-1] - slots[j-1])
-                    dp[i][j] = min(dp[i][j], dp[i-1][j-1] + cost)
+            for i in range(n + 1):
+                dp[i][j] = dp[i][j - 1]
 
-        return dp[R][F]
+                dist = 0
+                for k in range(1, min(limit, i) + 1):
+                    dist += abs(robot[i - k] - pos)
+                    dp[i][j] = min(dp[i][j], dp[i - k][j - 1] + dist)
+
+        return dp[n][m]
